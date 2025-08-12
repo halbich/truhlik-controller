be1355363bac66274361cc1a47c6785366e313f5
Pavel Halbich <halbich.p@hotmail.com>
2025-08-12 21:01:25 +0200

    update service, logs

--- services/relay.py
+++ services/relay.py
@@ -1,7 +1,7 @@
 import RPi.GPIO as GPIO
 
 #GPIO Pin
-Relay = [6, 13, 16, 19, 20, 21, 26] # 5 is first, but it is reserved
+Relay = [5, 6, 13, 16, 19, 20, 21, 26] # 5 is first, but it is reserved
 
 
 def init_relay():
@@ -10,7 +10,8 @@ def init_relay():
 
     for i in range(len(Relay)):
         GPIO.setup(Relay[i], GPIO.OUT)
-        GPIO.output(Relay[i], GPIO.HIGH)
+        set_relay(i, False)
 
 def set_relay(relay_id: int, is_on: bool):
     GPIO.output(Relay[relay_id], GPIO.LOW if is_on else GPIO.HIGH)
+    print(f"Relay {relay_id} is set to {is_on}")
